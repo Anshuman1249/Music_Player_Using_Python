1a4b9bbb9f56560bed8ed4015dc6883272728a75
Anshuman1249 <108320981+Anshuman1249@users.noreply.github.com>
2023-06-23 18:46:52 +0530

    Update Music_Player.py

--- Music_Player.py
+++ Music_Player.py
@@ -9,78 +9,76 @@ root.geometry("500x300")
 
 pygame.mixer.init()
 
-menubar= Menu(root)
+menubar = Menu(root)
 root.config(menu=menubar)
 
-songs =[]
+songs = []
 current_song = ""
 paused = False
 
 def load_music():
-	global current_song
-	root.directory = filedialog.askdirectory()
+    global current_song
+    root.directory = filedialog.askdirectory()
 
-	for song in os.listdir(root.directory):
-		name, ext= os.path.splitext(song)
-		if ext == '.mp3':
-			songs.append(song)
+    for song in os.listdir(root.directory):
+        name, ext = os.path.splitext(song)
+        if ext == '.mp3':
+            songs.append(song)
 
-	for song in songs:
-		songlist.insert("end", song)
-	
-	songlist.selection_set(0)
-	current_song = songs[songlist.curselection()[0]]		
+    for song in songs:
+        songlist.insert("end", song)
+
+    songlist.selection_set(0)
+    current_song = songs[songlist.curselection()[0]]
 
 def play_music():
-	global current_song, paused
+    global current_song, paused
 
-	if not paused:
-		pygame.mixer.load_music(os.path.join(root.askdirectory, current_song))
-		pygame.mixer.music.play()
-	else:
-		pygame.mixer.music.unpause()
-		paused = False
-			
+    if not paused:
+        pygame.mixer.music.load(os.path.join(root.directory, current_song))
+        pygame.mixer.music.play()
+    else:
+        pygame.mixer.music.unpause()
+        paused = False
 
 def pause_music():
-	global paused
-	pygame.mixer.music.paused()
-	paused = True
+    global paused
+    pygame.mixer.music.pause()
+    paused = True
 
 def next_music():
-	global current_song, paused
-	try:
-		songlist.selection_clear(0,END)
-		songlist.selection_set(songs.index(current_song) + 1)
-		current_song = songs[songlist.curselection()[0]]
-		play_music()
-	except:
-		pass
+    global current_song, paused
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song) + 1)
+        current_song = songs[songlist.curselection()[0]]
+        play_music()
+    except:
+        pass
 
 def prev_music():
-	global current_song, paused
+    global current_song, paused
 
-	try:
-		songlist.selection_clear(0,END)
-		songlist.selection_set(songs.index(current_song) - 1)
-		current_song = songs[songlist.curselection()[0]]
-		play_music()
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song) - 1)
+        current_song = songs[songlist.curselection()[0]]
+        play_music()
 
-	except:
-		pass
-
+    except:
+        pass
 
 organise_menu = Menu(menubar, tearoff=False)
-organise_menu.add_command(label='Select Folder', command=load_music	)
+organise_menu.add_command(label='Select Folder', command=load_music)
 menubar.add_cascade(label='organise', menu=organise_menu)
 
 songlist = Listbox(root, bg="white", fg="black", width=100, height=12)
 songlist.pack(pady=10)
 
-play_btn_img = PhotoImage(file='//put your location here//play.png')
-pause_btn_img = PhotoImage(file='//put your location here//pause.png')
-next_btn_img = PhotoImage(file='//put your location here//next.png')
-prev_btn_img = PhotoImage(file='//put your location here//previous.png')
+play_btn_img = PhotoImage(file='//home//sasuke//py//music player//play.png')
+pause_btn_img = PhotoImage(file='//home//sasuke//py//music player//pause.png')
+next_btn_img = PhotoImage(file='//home//sasuke//py//music player//next.png')
+prev_btn_img = PhotoImage(file='//home//sasuke//py//music player//previous.png')
 
 controls_frame = Frame(root)
 controls_frame.pack()
@@ -90,10 +88,9 @@ pause_btn = Button(controls_frame, image=pause_btn_img, borderwidth=0, command=p
 next_btn = Button(controls_frame, image=next_btn_img, borderwidth=0, command=next_music)
 prev_btn = Button(controls_frame, image=prev_btn_img, borderwidth=0, command=prev_music)
 
-play_btn.grid(row=0, column= 1, padx=7, pady=10)
-pause_btn.grid(row=0, column= 2, padx=7, pady=10)
-next_btn.grid(row=0, column= 3, padx=7, pady=10)
-prev_btn.grid(row=0, column= 0, padx=7, pady=10)
-
+play_btn.grid(row=0, column=1, padx=7, pady=10)
+pause_btn.grid(row=0, column=2, padx=7, pady=10)
+next_btn.grid(row=0, column=3, padx=7, pady=10)
+prev_btn.grid(row=0, column=0, padx=7, pady=10)
 
 root.mainloop()
